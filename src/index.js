f312ca0dc94baa30aa9fbf4bd83494cbb52c1274
Sagar <saagarkozu@gmail.com>
2026-07-16 10:27:11 +0545

    feat: set up Express server with MongoDB connection and error handling utilities
    - Added Express server setup in app.js with CORS and cookie-parser middleware.
    - Created a constants.js file to store database name.
    - Implemented MongoDB connection logic in a new db/index.js file.
    - Updated index.js to start the server after successful database connection.
    - Introduced ApiError and ApiResponse classes for standardized error and response handling.
    - Added asyncHandler utility for handling asynchronous route handlers.
    - Updated package.json to include necessary dependencies.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("Hello World");
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+import 'dotenv/config'
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 4000, ()=>{
+        console.log(`Server is runing at port: ${process.env.PORT}`)
+    })
+})
+.catch(e => {
+    console.log("MongoDB connection failed !", e)
+})
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js"
+// ;( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("Error connecting to MongoDB:", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         });
+
+//     } catch (error) {
+//         console.log("Error connecting to DB:", error);
+//         throw error;
+//     }
+// })
