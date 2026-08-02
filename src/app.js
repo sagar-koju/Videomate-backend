c08d30cf39b5b3847841894286dbafa1e694a48a
Sagar <saagarkozu@gmail.com>
2026-08-02 14:54:53 +0545

    fix: correct import path for error handler middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import {errorHandler} from "./middlewares/errorHandler.js"
+import {errorHandler} from "./middlewares/error.middleware.js"
 
 const app = express();
 
